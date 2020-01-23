954bce4a6091522331ce056c96d236929aafe2e5
diego-rz <diegorzuleta@gmail.com>
2020-01-22 21:28:59 -0300

    Avance agregar evento

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventDto } from '../model/event';
 
 @Component({
   selector: 'app-add-event',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
+  address: Address = {
+    street: '',
+    number: 0,
+    floor: 0,
+    department: '',
+    locality: '',
+    province: '',
+  };
+
+  date: Date = {
+    day: 0,
+    month: 0,
+    year: 0,
+    hour: 0,
+    minute: 0
+  }
+
+  event: EventDto = {
+    nombre: '',
+    tipo: '',
+    ciudad: 0,
+    fecha: '',
+    hora: '',
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export class Address {
+  street: string;
+  number: number;
+  floor: number;
+  department: string;
+  locality: string;
+  province: string;
+}
+
+export class Date {
+  day: number;
+  month: number;
+  year: number;
+  hour: number;
+  minute: number;
 }
