fc39eb3fc4cd9de9cebf9f477ea059cdb3e49c37
diego-rz <diegorzuleta@gmail.com>
2020-02-13 01:37:04 -0300

    eliminado boton de prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { EventComponent } from './event/event.component';
 import { CitiesPipe } from './cities.pipe';
 import { RatingsComponent } from './ratings/ratings.component';
 import { RateModalComponent } from './rate-modal/rate-modal.component';
-import { FileSelectDirective } from "ng2-file-upload";
+import { FileUploadModule } from "ng2-file-upload";
 import { FileUploaderComponent } from './file-uploader/file-uploader.component';
 
 @NgModule({
@@ -37,14 +37,14 @@ import { FileUploaderComponent } from './file-uploader/file-uploader.component';
     CitiesPipe,
     RatingsComponent,
     RateModalComponent,
-    FileSelectDirective,
     FileUploaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
